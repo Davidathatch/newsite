ebb5e5aeac39998eda143554f672763eb4608091
David Thatcher <54860843+Davidathatch@users.noreply.github.com>
2020-05-01 14:43:36 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+$("i").click(function () {
+  $("body").toggleClass("bg-dark font-dark");
+  $(".card-body").toggleClass("news-Card-Dark card-Body-Style");
+  $(".card").toggleClass("bg-dark");
+  $(".card img").toggleClass("card-Img-Dark");
+});
