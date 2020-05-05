355072818cf429add899b3f04d329b33678e8f3b
David Thatcher <54860843+Davidathatch@users.noreply.github.com>
2020-05-05 14:04:30 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-$("i").click(function () {
-  $("body").toggleClass("bg-dark font-dark");
-  $(".card-body").toggleClass("news-Card-Dark card-Body-Style");
-  $(".card").toggleClass("bg-dark");
-  $(".card img").toggleClass("card-Img-Dark");
+function toggleTheme() {
+  if (localStorage.getItem("pageThemeLocal") === "dark") {
+    $("body").addClass("body-bg-dark font-dark");
+    $(".card-body").addClass("news-Card-Dark");
+    $(".card-body").removeClass("card-Body-Style");
+    $(".card").addClass("bg-dark");
+    $(".card img").addClass("card-Img-Dark");
+    $(".recent-stories ul").addClass("card-bottom-sec");
+    $(".list-group li").addClass("btn-dark-mode");
+    $(".recent-stories.btn").addClass("btn-dark btn-secondary");
+    $(".navbar").addClass("navbar-dark bg-dark");
+    $(".footer").addClass("footer-dark");
+    $(".news-Card h5").addClass("sub-text-dark card-title");
+    $(".header-icon").addClass("header-icon-dark");
+  } else if (localStorage.getItem("pageThemeLocal") === "light") {
+    $("body").removeClass("body-bg-dark font-dark");
+    $(".card-body").removeClass("news-Card-Dark card-Body-Style");
+    $(".card-body").addClass("card-Body-Style");
+    $(".card").removeClass("bg-dark");
+    $(".card img").removeClass("card-Img-Dark");
+    $(".recent-stories ul").removeClass("card-bottom-sec");
+    $(".list-group li").removeClass("btn-dark-mode");
+    $(".recent-stories.btn").removeClass("btn-dark btn-secondary");
+    $(".navbar").removeClass("navbar-dark bg-dark");
+    $(".footer").removeClass("footer-dark");
+    $(".news-Card h5").removeClass("sub-text-dark card-title");
+    $(".header-icon").removeClass("header-icon-dark");
+  }
+}
+
+$(".dark-mode-toggler").click(function () {
+  if (localStorage.getItem("pageThemeLocal") === "light") {
+    var pageTheme = "dark";
+    localStorage.setItem("pageThemeLocal", "dark");
+  } else if (localStorage.getItem("pageThemeLocal") === "dark") {
+    var pageTheme = "light";
+    localStorage.setItem("pageThemeLocal", "light");
+  }
+  toggleTheme();
 });
+if (localStorage.getItem("pageThemeLocal") === null) {
+  localStorage.setItem("pageThemeLocal", "light");
+}
+toggleTheme();
